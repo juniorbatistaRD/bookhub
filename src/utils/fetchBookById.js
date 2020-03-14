18606c987b0a4b69b7bba1b2e859248320ce7a07
Junior <juniorabc77@gmail.com>
2020-03-14 12:14:48 -0400

    minor fix

--- src/utils/fetchBookById.js
+++ src/utils/fetchBookById.js
@@ -1,33 +1,24 @@
+const apiKey = process.env.REACT_APP_GOODREADS_KEY;
 
-const apiKey = "TsO04wQ6FrjjKBULTmj66w"
+const fetchBookById = async id => {
+  const response = await fetch(
+    ` https://www.goodreads.com/book/show.xml?id=${id}&key=${apiKey}`
+  );
+  const data = await response.text();
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(data, "text/xml");
+  const booksData = xml.getElementsByTagName("book");
 
+  const bookDataINeed = {
+    id: booksData[0].childNodes[1].innerHTML,
+    year: booksData[0].childNodes[21].innerHTML,
+    rating: booksData[0].childNodes[37].innerHTML,
+    title: booksData[0].childNodes[3].childNodes[0].wholeText,
+    author: booksData[0].childNodes[53].childNodes[1].childNodes[3].innerHTML,
+    image: booksData[0].childNodes[17].innerHTML
+  };
 
-const fetchBookById = async (id) => {
-    const response = await fetch(` https://www.goodreads.com/book/show.xml?id=${id}&key=${apiKey}`)
-    const data = await response.text()
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(data, 'text/xml')
-    const booksData = xml.getElementsByTagName("book")
+  return bookDataINeed;
+};
 
-    const bookDataINeed = {
-                id:booksData[0].childNodes[1].innerHTML,
-                year:booksData[0].childNodes[21].innerHTML,
-                rating:booksData[0].childNodes[37].innerHTML,
-                title:booksData[0].childNodes[3].childNodes[0].wholeText,
-                author:booksData[0].childNodes[53].childNodes[1].childNodes[3].innerHTML,
-                image:booksData[0].childNodes[17].innerHTML, 
-    }
-
-
-
-
-    return bookDataINeed
-}
-
-
-
-
-
-
-
-export default fetchBookById
+export default fetchBookById;
